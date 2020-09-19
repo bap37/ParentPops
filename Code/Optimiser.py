8aedf33fae41322b9a28512256d0fbb4ea9ad6d9
Brodie Popovic <bap37@duke.edu>
2020-09-19 14:52:46 -0400

    Upgraded Loading_Files, Matrix, and Optimiser for more general models than GGN

--- Code/Optimiser.py
+++ Code/Optimiser.py
@@ -1,4 +1,4 @@
-EXTRA_FUNC_FILEPATH = 'replaceme'
+EXTRA_FUNC_FILEPATH = 'replace me'
 
 import pandas as pd
 import numpy as np
@@ -12,32 +12,45 @@ parser=argparse.ArgumentParser()
 import emcee
 from scipy.stats import binned_statistic
 
-def Optimiser(Param, dfdata, DIM, dfpre, dfpost, binsize): #better explained in Optimiser_Mass
+def Optimiser(Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE): #better explained in Optimiser_Mass
     dfk = dfdata
+    DIM = len(SHAPE2)
     if Param == 'c':
         cI_m = MI.Matrix_c_init(dfpre, dfpost, binsize)
+        if DIM == 4:
+            nwalkers = 2*(DIM + 1) - 1
+            ndim = DIM - 1 
+        else: 
+            nwalkers = 2*(DIM + 1) 
+            ndim = DIM 
+        p0 = np.random.rand(nwalkers, ndim)
+        p0 = p0/100
+        p0 = np.abs(p0)    
     else:
         cI_m = MI.Matrix_x_init(dfpre, dfpost, binsize)
-    nwalkers = 2*(DIM + 1)
-    ndim = DIM
-    p0 = np.random.rand(nwalkers, ndim)
-    p0 = p0/100
-    p0 = np.abs(p0)
+        nwalkers = 2*(DIM + 1)
+        ndim = DIM
+        p0 = np.random.rand(nwalkers, ndim)
+        p0 = p0/100
+        p0 = np.abs(p0)
     if Param == 'c':
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, Matrix.Matrix_c, args=[dfk, cI_m, binsize])
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, Matrix.Matrix_c, args=[dfk, cI_m, binsize, SHAPE])
     else:
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, Matrix.Matrix_x, args=[dfk, cI_m, binsize])
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, Matrix.Matrix_x, args=[dfk, cI_m, binsize, SHAPE])
     state = sampler.run_mcmc(p0, 1000, progress=True)
     sampler.reset()
     sampler.run_mcmc(state, 10000, progress=True)
     samples = sampler.get_chain()
     print(Param)
-    print([np.mean(samples[:,:,0]), np.std(samples[:,:,0])])
-    print([np.mean(samples[:,:,1]), np.std(samples[:,:,1])])
-    print([np.mean(samples[:,:,2]), np.std(samples[:,:,2])])
-    print([np.mean(samples[:,:,3]), np.std(samples[:,:,3])])
+    for h in range((ndim)):
+        print([np.mean(samples[:,:,h]), np.std(samples[:,:,h])], SHAPE2[h])
+
     
 #Now we need to cycle through masses
+#masses = np.arange(8, 13.6, 0.2)
+#With window of 0.6 right now - eg 0.2 +/- 0.6 
+
+#If we want redshift, it'll need to be in log scale - get back to you on that
 
 
 
