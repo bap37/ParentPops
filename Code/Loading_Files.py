7c701bd140fb4c2c9e19200e927880d6c3336d43
Lisa Lu <lisalu.may@gmail.com>
2020-09-24 23:19:45 -0400

    Basic Cleanup

--- Code/Loading_Files.py
+++ Code/Loading_Files.py
@@ -1,7 +1,6 @@
 #Define config variables up here
 #Right now this only works when run in command line with the relevant arguments.
 
-#EXTRA_FUNC_FILEPATH = 'replaceme' #'I need to point to ParentPop/Code/'
 REF_FP = "replaceme" #I need to point to the unzipped version of REF.zip!'
 if REF_FP == "replaceme":
     print("Did not replace REF_FP with unzipped REF folder.")
@@ -164,8 +163,8 @@ for num,s in enumerate(SURVEY):
 print(len(DATOT))
 
 import Optimiser
-#Optimiser.Optimiser(Param,DATOT, SHAPE2, dfpre, dfpost, .2, SHAPE)
+
 optimizer = Optimiser.Optimizer_Calculation()
-optimizer.optimize_secondary(Param,DATOT, SHAPE2, dfpre, dfpost, .2, SHAPE, None) #COMMENT THIS OUT AND UNCOMMENT THE ONE BELOW IF DOING  MASS RANGE
+optimizer.optimize(Param,DATOT, SHAPE2, dfpre, dfpost, .2, SHAPE, None) #COMMENT THIS OUT AND UNCOMMENT THE ONE BELOW IF DOING  MASS RANGE
 #optimizer.optimize_secondary_in_range(Param,DATOT, SHAPE2, dfpre, dfpost, .2, SHAPE)
 
--- Code/Optimiser.py
+++ Code/Optimiser.py
@@ -3,7 +3,6 @@ EXTRA_FUNC_FILEPATH = 'replace me'
 import pandas as pd
 import numpy as np
 import sys
-#sys.path.insert(0, EXTRA_FUNC_FILEPATH) 
 import Functions #I will need to include this
 import MI
 import Matrix
@@ -11,6 +10,8 @@ import argparse
 parser=argparse.ArgumentParser()
 import emcee
 from scipy.stats import binned_statistic
+import os
+import shutil
 
 def Optimiser(Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE): #better explained in Optimiser_Mass
     dfk = dfdata
@@ -53,39 +54,9 @@ def Optimiser(Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE): #better exp
 #If we want redshift, it'll need to be in log scale - get back to you on that
 
 class Optimizer_Calculation:
-    
-    def optimize(self, Param, dfdata, DIM, dfpre, dfpost, binsize, MASS):
-        dfk = dfdata
-        if MASS != None:
-            window = 0.6
-            dfk = dfk.loc[(dfk.HOST_LOGMASS < MASS +window) & (dfk.HOST_LOGMASS >= MASS - window)]
-        if Param == 'c':
-            cI_m = MI.Matrix_c_init(dfpre, dfpost, binsize)
-        else:
-            cI_m = MI.Matrix_x_init(dfpre, dfpost, binsize)
-        nwalkers = 2*(DIM + 1)
-        ndim = DIM
-        p0 = np.random.rand(nwalkers, ndim)
-        p0 = p0/100
-        p0 = np.abs(p0)
-        if Param == 'c':
-            sampler = emcee.EnsembleSampler(nwalkers, ndim, Matrix.Matrix_c, args=[dfk, cI_m, binsize])
-        else:
-            sampler = emcee.EnsembleSampler(nwalkers, ndim, Matrix.Matrix_x, args=[dfk, cI_m, binsize])
-        state = sampler.run_mcmc(p0, 1000, progress=True)
-        sampler.reset()
-        sampler.run_mcmc(state, 10000, progress=True)
-        samples = sampler.get_chain()
-        print(Param)
-        print([np.mean(samples[:,:,0]), np.std(samples[:,:,0])]) # 0 == mean
-        print([np.mean(samples[:,:,1]), np.std(samples[:,:,1])]) # stdl
-        print([np.mean(samples[:,:,2]), np.std(samples[:,:,2])]) # stdr
-        print([np.mean(samples[:,:,3]), np.std(samples[:,:,3])]) # n
         # TODO: Tab deliniation csv? If do arange == true, append to the tsv output of each iteration (m is default HOST_LOGMASS from dfdata, otherwise the arrange)
         # TODO: mean, stdl, stdr, n, m
-        return [[],[],[],[],[]]
-
-    def optimize_secondary(self, Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE, MASS):
+    def optimize(self, Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE, MASS):
         dfk = dfdata
         if MASS != None:
             window = 0.6
@@ -126,42 +97,24 @@ class Optimizer_Calculation:
         
         return return_list
         
-        
     def write_to_file(self):
+        if not os.path.exists("output"):
+            os.mkdir("output")
+        else:
+            shutil.rmtree("output")
+            os.mkdir("output")
+            
         print("Written To File") #Also do check for if multiple surveys and also print that note in the file name
-
-    def optimize_in_range(self, Param, dfdata, DIM, dfpre, dfpost, binsize, MASS, Shape):
-        mean = []
-        stdl= []
-        stdr = []
-        n = []
-        m = []
-        for m in np.arange(6,14,.2):
-            calculation = self.optimize(Param, dfdata, DIM, dfpre, dfpost, binsize, m) # RETURNS: An array of arrays, for mean, stdl, stdr, n, m
-            mean = mean.append(calculation[0])
-            stdl = stdl.append(calculation[1])
-            stdr = stdr.append(calculation[2])
-            n = n.append(calculation[3])
-            m = m.append(calculation[4])
-        return [mean, stdl, stdr, n, m] #Allows us potentially to print from here
     
-    def optimize_secondary_in_range(self, Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE):
+    def optimize_in_range(self, Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE):
         collected_result = []
         for i in SHAPE2:
             collected_result.append([])
         
         for m in np.arange(6, 14, .2):
-            calculation = self.optimize_secondary(Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE, m)
+            calculation = self.optimize(Param, dfdata, SHAPE2, dfpre, dfpost, binsize, SHAPE, m)
             
             for n in range(0, len(calculation) - 1):
                 collected_result[n].append(calculation[n])
         
         return collected_result
-
-
-""" 
-
-if __name__ == "__main__": 
-    Optimiser_Mass()
-
- """
